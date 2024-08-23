0a7038d8c4a63fa3c1c50309eaeb058a5c086ac7
Farzan Hashmi <farzan.ahmad.hashmi@gmail.com>
2024-08-23 00:00:41 -0700

    higher level apis

---
+++ higher_level_bisect_apis.py
@@ -0,0 +1,32 @@
+# This is terrible. I should be able to logic this out in like 5 seconds but alas.
+
+
+from bisect import bisect_left, bisect_right
+
+
+def next_smaller(arr, value):
+    """Return strictly smaller value to the given value."""
+    i = bisect_left(value) - 1
+    if i < 0:
+        return None
+    return arr[i]
+
+def floor(arr, value):
+    """Return smaller or equal value than the given value."""
+    i = bisect_right(value) - 1
+    if i < 0:
+        return None
+    return arr[i]
+
+def ceil(arr, value):
+    """Return larger or equal value than the given value."""
+    i = bisect_left(value)
+    if i >= len(arr):
+        return None
+    return arr[i]
+
+def next_greater(arr, value):
+    """Return strictly larger value than the given value."""
+    i = bisect_right(arr, value)
+    if i >= len(arr):
+        return None
